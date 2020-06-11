f9eb312255c5c2d03a207610334c9511dec30798
sameerisa <sameersamisa@gmail.com>
2020-06-11 01:51:51 -0400

    no change

--- meeting1.bat
+++ meeting1.bat
@@ -1,6 +1,7 @@
 @echo off
 taskkill /F /IM TeamViewer.exe
 
+
 powershell -command "(new-object -com shell.application).minimizeall();
 
 for /f "delims== tokens=1,2,3,4,5,6" %%G in (C:\Users\samee\Documents\zoom_auto_join\meeting_details.txt) do set %%G=%%H
