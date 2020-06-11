a8020818cbf6893fb94d8a5286abb8dc1c64668c
sameerisa <sameersamisa@gmail.com>
2020-06-11 01:54:20 -0400

    Update meeting3.bat
    Monitor off

--- meeting3.bat
+++ meeting3.bat
@@ -11,3 +11,5 @@ for /f "delims== tokens=1,2,3,4,5,6" %%G in (C:\Users\samee\Documents\zoom_auto_
 python C:\Users\samee\Documents\zoom_auto_join\zoom_join.py --i=%PARAM5% --p=%PARAM6%
 
 start C:\"Program Files (x86)"\TeamViewer\TeamViewer.exe
+
+start turnoffmonitor.bat
