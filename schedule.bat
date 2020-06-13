77a1d2d545d8a1de97c590fb7882bc0a3f7b2ca8
sameerisa <sameersamisa@gmail.com>
2020-06-12 23:03:17 -0400

    Update schedule.bat
    changed schedule

--- schedule.bat
+++ schedule.bat
@@ -1,10 +1,10 @@
 @echo off
-schtasks /create /sc DAILY /tn schedule1 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 02:18 /f
-schtasks /create /sc DAILY /tn kill1 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 04:15 /f
-schtasks /create /sc DAILY /tn schedule2 /tr C:\Users\samee\Documents\zoom_auto_join\meeting2.bat /st 04:18 /f
-schtasks /create /sc DAILY /tn kill2 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 05:15 /f
+schtasks /create /sc DAILY /tn schedule1 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 12:18 /f
+schtasks /create /sc DAILY /tn kill1 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 02:15 /f
+schtasks /create /sc DAILY /tn schedule2 /tr C:\Users\samee\Documents\zoom_auto_join\meeting2.bat /st 02:18 /f
+schtasks /create /sc DAILY /tn kill2 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 04:15 /f
 rem schtasks /create /sc DAILY /tn schedule3 /tr C:\Users\samee\Documents\zoom_auto_join\meeting3.bat /st 04:18 /f
 rem schtasks /create /sc DAILY /tn kill3 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 05:15 /f
 rem schtasks /create /sc DAILY /tn schedule4 /tr C:\Users\samee\Documents\zoom_auto_join\meeting4.bat /st 03:18 /f
 rem schtasks /create /sc DAILY /tn kill4 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 05:00 /f
-schtasks /create /sc DAILY /tn shutdown /tr C:\Users\samee\Documents\zoom_auto_join\shutdown.bat /st 05:16 /f
+schtasks /create /sc DAILY /tn shutdown /tr C:\Users\samee\Documents\zoom_auto_join\shutdown.bat /st 04:16 /f
