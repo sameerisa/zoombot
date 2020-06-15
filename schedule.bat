e8fbae4bdedf10ffed0ffd530fe636462e8b2d98
sameerisa <sameersamisa@gmail.com>
2020-06-15 00:40:17 -0400

    bug

--- schedule.bat
+++ schedule.bat
@@ -1,5 +1,5 @@
 @echo off
-schtasks /create /sc DAILY /tn schedule1 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 12:18 /f
+schtasks /create /sc DAILY /tn schedule1 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 00:18 /f
 schtasks /create /sc DAILY /tn kill1 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 02:15 /f
 schtasks /create /sc DAILY /tn schedule2 /tr C:\Users\samee\Documents\zoom_auto_join\meeting2.bat /st 02:18 /f
 schtasks /create /sc DAILY /tn kill2 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 04:15 /f
