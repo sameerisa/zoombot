abe5edc9f4d56902b5864f9a2c629c40675e8ef7
sameer <sameersamisa@gmail.com>
2020-06-22 10:58:28 -0400

    routine

--- clear_schedule.bat
+++ clear_schedule.bat
@@ -3,8 +3,8 @@ schtasks /delete /tn schedule1 /f
 schtasks /delete /tn schedule2 /f
 schtasks /delete /tn schedule3 /f
 schtasks /delete /tn schedule4 /f
-schtasks /delete /tn kill1 /f
-schtasks /delete /tn kill2 /f
-schtasks /delete /tn kill3 /f
-schtasks /delete /tn kill4 /f
+rem schtasks /delete /tn kill1 /f
+rem schtasks /delete /tn kill2 /f
+rem schtasks /delete /tn kill3 /f
+rem schtasks /delete /tn kill4 /f
 schtasks /delete /tn shutdown /f
--- schedule.bat
+++ schedule.bat
@@ -13,4 +13,4 @@ rem schtasks /create /sc DAILY /tn kill1 /tr C:\Users\samee\Documents\zoom_auto_
 rem schtasks /create /sc DAILY /tn kill4 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 05:00 /f
 rem schtasks /create /sc DAILY /tn kill2 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 02:15 /f
 
-schtasks /create /sc DAILY /tn shutdown /tr C:\Users\samee\Documents\zoom_auto_join\shutdown.bat /st 04:00 /f
+schtasks /create /sc DAILY /tn shutdown /tr C:\Users\samee\Documents\zoom_auto_join\shutdown.bat /st 06:00 /f
