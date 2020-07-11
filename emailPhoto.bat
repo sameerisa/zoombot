0e2b6cc877dfad387553b1f9f77170993ab3d63e
sameer <sameersamisa@gmail.com>
2020-07-11 01:13:19 -0400

    close teamviewer on screenshot

--- emailPhoto.bat
+++ emailPhoto.bat
@@ -3,6 +3,8 @@ if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && 
 
 cd C:\Users\samee\Documents\zoom_auto_join\
 
+taskkill /F /IM TeamViewer.exe
+
 nircmd exec hide startEmailServer.bat
 
 timeout /t 5
@@ -14,6 +16,8 @@ nircmd.exe savescreenshot Capture.png
 python emailPhoto.py
 
 timeout /t 5
+
+start C:\"Program Files (x86)"\TeamViewer\TeamViewer.exe
 
 start turnoffmonitor.bat
 
