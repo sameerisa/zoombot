91075d7014c21a4577d4828ca0cc1e8f7b763d3c
sameerisa <sameersamisa@gmail.com>
2020-07-21 21:30:05 -0400

    Update schedule.bat
    Time change

--- schedule.bat
+++ schedule.bat
@@ -1,6 +1,6 @@
 @echo off
 schtasks /create /sc DAILY /tn schedule1 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 00:18 /f
-schtasks /create /sc DAILY /tn schedule2 /tr C:\Users\samee\Documents\zoom_auto_join\meeting2.bat /st 02:18 /f
+schtasks /create /sc DAILY /tn schedule2 /tr C:\Users\samee\Documents\zoom_auto_join\meeting2.bat /st 01:55 /f
 schtasks /create /sc DAILY /tn schedule3 /tr C:\Users\samee\Documents\zoom_auto_join\meeting3.bat /st 04:18 /f
 rem schtasks /create /sc DAILY /tn schedule4 /tr C:\Users\samee\Documents\zoom_auto_join\meeting4.bat /st 03:18 /f
 
