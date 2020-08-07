35c644a612c9cf40e4569b5263a9e5d967ac7314
sameerisa <sameersamisa@gmail.com>
2020-08-06 21:48:21 -0400

    Update schedule.bat

--- schedule.bat
+++ schedule.bat
@@ -1,13 +1,13 @@
 @echo off
 schtasks /create /sc DAILY /tn schedule1 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 23:50 /f
 schtasks /create /sc DAILY /tn schedule2 /tr C:\Users\samee\Documents\zoom_auto_join\meeting2.bat /st 01:18 /f
-schtasks /create /sc DAILY /tn schedule3 /tr C:\Users\samee\Documents\zoom_auto_join\meeting3.bat /st 02:50 /f
-schtasks /create /sc DAILY /tn schedule4 /tr C:\Users\samee\Documents\zoom_auto_join\meeting4.bat /st 04:20 /f
+rem schtasks /create /sc DAILY /tn schedule3 /tr C:\Users\samee\Documents\zoom_auto_join\meeting3.bat /st 02:50 /f
+rem schtasks /create /sc DAILY /tn schedule4 /tr C:\Users\samee\Documents\zoom_auto_join\meeting4.bat /st 04:20 /f
 
 schtasks /create /sc DAILY /tn monitor1 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 00:30 /f
 schtasks /create /sc DAILY /tn monitor2 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 01:45 /f
-schtasks /create /sc DAILY /tn monitor3 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 03:30 /f
-schtasks /create /sc DAILY /tn monitor4 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 05:00 /f
+rem schtasks /create /sc DAILY /tn monitor3 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 03:30 /f
+rem schtasks /create /sc DAILY /tn monitor4 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 05:00 /f
 
 rem schtasks /create /sc DAILY /tn killcmd1 /tr C:\Users\samee\Documents\zoom_auto_join\killcmd.bat /st 00:20 /f
 rem schtasks /create /sc DAILY /tn killcmd2 /tr C:\Users\samee\Documents\zoom_auto_join\killcmd.bat /st 02:20 /f
@@ -24,4 +24,4 @@ rem schtasks /create /sc DAILY /tn kill1 /tr C:\Users\samee\Documents\zoom_auto_
 rem schtasks /create /sc DAILY /tn kill4 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 05:00 /f
 rem schtasks /create /sc DAILY /tn kill2 /tr C:\Users\samee\Documents\zoom_auto_join\kill.bat /st 02:15 /f
 
-schtasks /create /sc DAILY /tn shutdown /tr C:\Users\samee\Documents\zoom_auto_join\shutdown.bat /st 06:00 /f
+schtasks /create /sc DAILY /tn shutdown /tr C:\Users\samee\Documents\zoom_auto_join\shutdown.bat /st 03:00 /f
