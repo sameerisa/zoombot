9be3c87360d5160bb020f8f7d584223d16b2fa06
sameerisa <sameersamisa@gmail.com>
2021-01-31 21:14:34 -0500

    daily update

--- schedule.bat
+++ schedule.bat
@@ -1,13 +1,13 @@
 @echo off
-schtasks /create /sc DAILY /tn schedule1 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 23:50 /f
-schtasks /create /sc DAILY /tn schedule2 /tr C:\Users\samee\Documents\zoom_auto_join\meeting2.bat /st 01:18 /f
+schtasks /create /sc DAILY /tn schedule1 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 22:50 /f
+schtasks /create /sc DAILY /tn schedule2 /tr C:\Users\samee\Documents\zoom_auto_join\meeting2.bat /st 00:18 /f
 schtasks /create /sc DAILY /tn schedule3 /tr C:\Users\samee\Documents\zoom_auto_join\meeting3.bat /st 02:50 /f
-schtasks /create /sc DAILY /tn schedule4 /tr C:\Users\samee\Documents\zoom_auto_join\meeting4.bat /st 04:20 /f
+rem schtasks /create /sc DAILY /tn schedule4 /tr C:\Users\samee\Documents\zoom_auto_join\meeting4.bat /st 04:20 /f
 
-schtasks /create /sc DAILY /tn monitor1 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 00:30 /f
+schtasks /create /sc DAILY /tn monitor1 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 23:00 /f
 schtasks /create /sc DAILY /tn monitor2 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 01:45 /f
 schtasks /create /sc DAILY /tn monitor3 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 03:30 /f
-schtasks /create /sc DAILY /tn monitor4 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 05:00 /f
+rem schtasks /create /sc DAILY /tn monitor4 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 05:00 /f
 
 rem schtasks /create /sc DAILY /tn killcmd1 /tr C:\Users\samee\Documents\zoom_auto_join\killcmd.bat /st 00:20 /f
 rem schtasks /create /sc DAILY /tn killcmd2 /tr C:\Users\samee\Documents\zoom_auto_join\killcmd.bat /st 02:20 /f
