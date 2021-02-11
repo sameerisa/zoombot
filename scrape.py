d64f11efa71ad4f7505d906a91879fe6f6419cba
sameerisa <sameersamisa@gmail.com>
2021-02-10 22:25:23 -0500

    Update scrape.py
    content switch

--- scrape.py
+++ scrape.py
@@ -68,9 +68,9 @@ def main():
     content1, content2 = parts
     #content1, content2, content3 = parts
 
-    print(content1)
+    print(content2)
     #print(type(content1))
-    body = content1.as_string()
+    body = content2.as_string()
     #print(body)
     #print(type(body))
     body = body.replace('*', '')
