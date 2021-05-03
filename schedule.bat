3ac21c405d188aad6baef13c9a634c1fe8e7bcf2
sameerisa <sameersamisa@gmail.com>
2021-05-03 13:48:07 -0400

    Update schedule.bat
    fix sched

--- schedule.bat
+++ schedule.bat
@@ -1,13 +1,13 @@
 @echo off
-schtasks /create /sc DAILY /tn schedule0 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 23:00 /f
-schtasks /create /sc DAILY /tn schedule1 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 23:50 /f
-schtasks /create /sc DAILY /tn schedule2 /tr C:\Users\samee\Documents\zoom_auto_join\meeting2.bat /st 01:20 /f
-schtasks /create /sc DAILY /tn schedule3 /tr C:\Users\samee\Documents\zoom_auto_join\meeting3.bat /st 02:50 /f
+rem schtasks /create /sc DAILY /tn schedule0 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 23:00 /f
+schtasks /create /sc DAILY /tn schedule1 /tr C:\Users\samee\Documents\zoom_auto_join\meeting1.bat /st 23:20 /f
+schtasks /create /sc DAILY /tn schedule2 /tr C:\Users\samee\Documents\zoom_auto_join\meeting2.bat /st 00:20 /f
+schtasks /create /sc DAILY /tn schedule3 /tr C:\Users\samee\Documents\zoom_auto_join\meeting3.bat /st 01:50 /f
 rem schtasks /create /sc DAILY /tn schedule4 /tr C:\Users\samee\Documents\zoom_auto_join\meeting4.bat /st 04:20 /f
 
-schtasks /create /sc DAILY /tn monitor1 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 00:10 /f
-schtasks /create /sc DAILY /tn monitor2 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 01:45 /f
-schtasks /create /sc DAILY /tn monitor3 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 03:10 /f
+schtasks /create /sc DAILY /tn monitor1 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 23:40 /f
+schtasks /create /sc DAILY /tn monitor2 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 00:30 /f
+schtasks /create /sc DAILY /tn monitor3 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 2:10 /f
 rem schtasks /create /sc DAILY /tn monitor4 /tr C:\Users\samee\Documents\zoom_auto_join\emailPhoto.bat /st 05:00 /f
 
 rem schtasks /create /sc DAILY /tn killcmd1 /tr C:\Users\samee\Documents\zoom_auto_join\killcmd.bat /st 00:20 /f
